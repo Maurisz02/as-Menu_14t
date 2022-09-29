c5d34c8c279f885b842c3a8dedba37d87418aa98
Pinter Adam <p2002adam@gmail.com>
2022-09-29 14:04:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menu_14t/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.menu_14t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSubmit;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+        editText = findViewById(R.id.editTextInput);
+    }
+}
