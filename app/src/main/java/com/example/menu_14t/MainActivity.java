267f5e566755113b1ff90fbced63446f835c7569
Pinter Adam <p2002adam@gmail.com>
2022-09-29 14:43:51 +0200

    orai kesz

--- app/src/main/java/com/example/menu_14t/MainActivity.java
+++ app/src/main/java/com/example/menu_14t/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.menu_14t;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +20,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1. elmentjuk
+                adatMentes();
+
+            }
+        });
+    }
+
+    private void adatMentes() {
+        SharedPreferences sp = getSharedPreferences("Data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        String seged= editText.getText().toString();
+        if(seged.equals(null)){
+            editText.setError("Muszáj kitölteni");
+        }else{
+            editor.putString("editTextErteke",seged);
+            //editor.commit();//return bool
+            editor.apply();
+
+            //2. atmegyunk masik activity be
+            Intent intent = new Intent(MainActivity.this,MasodikActivity2.class);
+            startActivity(intent);
+            //backStack
+            finish();
+        }
+
     }
 
     private void init(){
--- app/src/main/java/com/example/menu_14t/MasodikActivity2.java
+++ app/src/main/java/com/example/menu_14t/MasodikActivity2.java
@@ -2,13 +2,52 @@ package com.example.menu_14t;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MasodikActivity2 extends AppCompatActivity {
+
+    private TextView textViewResult;
+    private Button buttonResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masodik2);
+        init();
+
+        String seged = getSharedPreferenceData();
+        textViewResult.setText(seged);
+
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //felugro ablak
+                Toast.makeText(MasodikActivity2.this, "Viszlát!", Toast.LENGTH_SHORT).show();
+
+
+                Intent intent = new Intent(MasodikActivity2.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private String getSharedPreferenceData() {
+        SharedPreferences sp = getSharedPreferences("Data", Context.MODE_PRIVATE);
+        String result = sp.getString("editTextErteke","Nincs ilyen érték");
+        return result;
+    }
+
+    private void init(){
+        textViewResult = findViewById(R.id.textViewResult);
+        buttonResult = findViewById(R.id.buttonBack);
     }
 }
